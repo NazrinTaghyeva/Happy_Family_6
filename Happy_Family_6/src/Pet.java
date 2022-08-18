dda56e5db90eceb9cfd2d8de7cc03bc57202c100
Nazryn <80715402+NazrinTaghyeva@users.noreply.github.com>
2022-08-18 13:50:14 +0400

    Update Pet.java

--- Happy_Family_6/src/Pet.java
+++ Happy_Family_6/src/Pet.java
@@ -85,6 +85,9 @@ public class Pet {
                 ", habits=" + Arrays.toString(habits) +
                 '}';
     }
+//   public void finalize()  {
+//        System.out.println(nickName);
+//    }
 
 
 }
